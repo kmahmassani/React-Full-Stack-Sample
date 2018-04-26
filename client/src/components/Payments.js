615c959ae0e18692a25c6c8d30c784b1cbcbf0fd
Kamal Mahmassani <kmahamassani@gmail.com>
2018-04-26 15:46:30 +0300

    Added heroku postbuild

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,17 +5,18 @@ import * as actions from '../actions';
 
 class Payments extends Component {
   render() {
-    return (<StripeCheckout
-      name="Emaily"
-      description="$5 for 5 email credits"
-      amount={500}
-      token={token => this.props.handleToken(token)}
-      stripeKey={process.env.REACT_APP_STRIPE_KEY}
-    >
-      <button className="btn">
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">
                     Add Credits
-      </button>
-    </StripeCheckout>);
+        </button>
+      </StripeCheckout>);
   }
 }
 
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,7 +4,7 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
-    const charge = await stripe.charges.create({
+    await stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: '$5 for 5 credits',
